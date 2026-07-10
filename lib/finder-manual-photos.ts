30e1532cc28ac7bf48d88f1ae88b01babed0e2a8
Ro008 <rociosirvent@gmail.com>
2026-07-10 13:44:18 +0300

    feat(finder): update manual directory links for preserved photos
    - Replaced old Google Maps links with updated links in the manual directory for preserved photos.
    - Ensured consistency in the mapping of photos to their respective entries in the finder manual photos file.

--- lib/finder-manual-photos.ts
+++ lib/finder-manual-photos.ts
@@ -11,10 +11,14 @@
  * See `.cursor/rules/manual-directory-preserved-photos.mdc`.
  */
 const FINDER_MANUAL_PHOTOS_BY_MAPS_LINK: Record<string, string> = {
-  "https://maps.app.goo.gl/RDejcWME7Xr6tZtM6": "/finder/manual-photos/valentina-oflidou.png",
-  "https://maps.app.goo.gl/qmagUZLsBgrBreXE9": "/finder/manual-photos/vera-politou.png",
-  "https://maps.app.goo.gl/FwGDUQnrbQKUMasn8": "/finder/manual-photos/korina-tryfonos.png",
-  "https://maps.app.goo.gl/6uNG7ajS1UeZU96Q7": "/finder/manual-photos/georgina-sarika.png",
+  "https://maps.google.com/?cid=1665628448012477822&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQQAhgEIAA":
+    "/finder/manual-photos/valentina-oflidou.png",
+  "https://maps.google.com/?cid=15591583625893490566&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQQAhgEIAA":
+    "/finder/manual-photos/vera-politou.png",
+  "https://maps.google.com/?cid=9115583058084629147&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQQAhgEIAA":
+    "/finder/manual-photos/korina-tryfonos.png",
+  "https://maps.google.com/?cid=14943462510182054898&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQQAhgEIAA":
+    "/finder/manual-photos/georgina-sarika.png",
 };
 
 function normalizeMapsLink(link: string): string {
